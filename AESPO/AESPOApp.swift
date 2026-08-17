e8efb0e0755d601837862fa34628ede7046696c9
Matthew Deng <m29deng@uwaterloo.ca>
2026-08-16 21:28:23 -0400

    Initial Commit

---
+++ AESPO/AESPOApp.swift
@@ -0,0 +1,17 @@
+//
+//  AESPOApp.swift
+//  AESPO
+//
+//  Created by Matthew Deng on 2026-08-16.
+//
+
+import SwiftUI
+
+@main
+struct AESPOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AESPO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AESPO
+//
+//  Created by Matthew Deng on 2026-08-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
